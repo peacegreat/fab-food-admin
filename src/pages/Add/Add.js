235c80f2d9d6b75c894a72e12cbd4b69b217d42b
peacegreat <nanbyenlazwan@gmail.com>
2024-07-29 13:17:34 +0100

    list.js

--- src/pages/Add/Add.js
+++ src/pages/Add/Add.js
@@ -4,8 +4,8 @@ import { assets } from '../../components/admin_assets/assets'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
-const Add = () => {
-    const url = "http://localhost:4000"
+const Add = ({url}) => {
+   
     const [image,setImage] = useState(false)
     const [data,setData] = useState({
             name:"",
@@ -28,7 +28,7 @@ const Add = () => {
                 formData.append("price",Number(data.price))
                 formData.append("category",data.category)
                 formData.append("image",data.image)
-                const response = await axios.post(`${url}/api/food/add`,formData)
+                const response = await axios.post (`${url}/api/food/add`,formData)
                 if (response.data.success){
                     setData({
                         name:"",
